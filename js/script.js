1bf6e06c681f4651535dcdaab9713bf29d358471
Leofrpking <140731847+Leofrpking@users.noreply.github.com>
2023-08-15 08:53:08 +0545

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+function myfunction()
+{
+	var x =document.getElementById("pass");
+    
+    if(x.type === "password")
+    {
+        x.type = "text";  
+    }
+    else
+
+    {
+    	x.type = "password";
+    }
+}
+function validate(){
+	var password = document.getElementById("pass");
+	var length = document.getElementById("length");
+
+	if (password.value.length >=8) {
+		alert("login Succesfull");
+		window.location.replace("index.html");
+		return false;
+	}
+	else{
+		alert("login Failed");
+	}   
+}
+
